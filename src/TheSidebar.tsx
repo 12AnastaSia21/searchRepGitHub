9e0e80435ec737edaf6b1b8b24f754af03a024d5
Anastasia <maly1204n@gmail.com>
2024-08-22 21:10:19 +0300

    Sidebar-null

--- src/TheSidebar.tsx
+++ src/TheSidebar.tsx
@@ -4,27 +4,33 @@ import { Typography } from "@mui/material";
 import "./TheSidebar.sass";
 
 export default function TheSidebar() {
-    return (
-    <aside className="sidebar-container">
-      <h2>Название проекта</h2>
-      <div className="basic-info">
-        <Chip label="Язык" color="primary" />
-        <div className="star-info">
-          <StarIcon />
-          <Typography className="number-of-stars" variant="body2" gutterBottom>
-            9 800 000
-          </Typography>
-        </div>
-      </div>
-      <div className="info-container">
-        <Chip label="Технология" />
-        <Chip label="технол" />
-        <Chip label="тех" />
-        <Chip label="Техн" />
-      </div>
-      <Typography variant="body2" gutterBottom>
-        Лицензия
-      </Typography>
-    </aside>
-  );
+    //Вариант saidebar, когда не выбран репозиторий
+    /*return (
+        <aside className="sidebar-null">
+            <Typography style={{color: '#4F4F4F'}} variant="subtitle1" gutterBottom>Выберите репозиторий</Typography>
+        </aside>
+    )*/
+     return (
+     <aside className="sidebar-container">
+       <h2>Название проекта</h2>
+       <div className="basic-info">
+         <Chip label="Язык" color="primary" />
+         <div className="star-info">
+           <StarIcon />
+           <Typography className="number-of-stars" variant="body2" gutterBottom>
+             9 800 000
+           </Typography>
+         </div>
+       </div>
+       <div className="info-container">
+         <Chip label="Технология" />
+         <Chip label="технол" />
+         <Chip label="тех" />
+         <Chip label="Техн" />
+       </div>
+       <Typography variant="body2" gutterBottom>
+         Лицензия
+       </Typography>
+     </aside>
+   );
 }
