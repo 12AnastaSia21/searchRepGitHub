a67f591701231493622425c6f29d8cf9006134a7
Anastasia <maly1204n@gmail.com>
2024-08-22 18:23:08 +0300

    Creating a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.sass";
 import SearchAppBar from "./searchAppBar";
 import TheFooter from "./TheFooter";
+import TheSidebar from "./TheSidebar";
 
 function App() {
   return (
     <>
       <div className="app-container">
+        <SearchAppBar />
         <div className="body">
-          <SearchAppBar />
           <div className="content"></div>
+          <TheSidebar />
         </div>
         <TheFooter />
       </div>
---
+++ src/TheSidebar.tsx
@@ -0,0 +1,30 @@
+import Chip from "@mui/material/Chip";
+import StarIcon from "@mui/icons-material/Star";
+import { Typography } from "@mui/material";
+import "./TheSidebar.sass";
+
+export default function TheSidebar() {
+  return (
+    <aside className="sidebar-container">
+      <h2>Название проекта</h2>
+      <div className="basic-info">
+        <Chip label="Язык" color="primary" />
+        <div className="star-info">
+          <StarIcon />
+          <Typography className="number-of-stars" variant="body2" gutterBottom>
+            9 800 000
+          </Typography>
+        </div>
+      </div>
+      <div className="info-container">
+        <Chip label="Технология" />
+        <Chip label="технол" />
+        <Chip label="тех" />
+        <Chip label="Техн" />
+      </div>
+      <Typography variant="body2" gutterBottom>
+        Лицензия
+      </Typography>
+    </aside>
+  );
+}
