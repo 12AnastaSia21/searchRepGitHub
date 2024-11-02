377065c5f46fe2a1b9de60d6e2f7bbac860129a7
Anastasia <maly1204n@gmail.com>
2024-11-02 15:57:47 +0300

    Added search by pressing the Enter key

--- src/TheSearchAppBar.tsx
+++ src/TheSearchAppBar.tsx
@@ -1,36 +1,56 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from './stores/store';
-import { fetchRepositories } from './stores/slices/githubSlice';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "./stores/store";
+import { fetchRepositories } from "./stores/slices/githubSlice";
 import Button from "@mui/material/Button";
-import './TheSearchAppBar.sass';
+import "./TheSearchAppBar.sass";
 
 interface TheSearchAppBarProps {
-  onSearch: (query: string) => void; // Изменяем тип onSearch
+  onSearch: (query: string) => void;
 }
 
 export default function TheSearchAppBar({ onSearch }: TheSearchAppBarProps) {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const dispatch: AppDispatch = useDispatch();
 
   const handleSearch = () => {
     if (query) {
       dispatch(fetchRepositories(query));
-      onSearch(query); // Передаем query в onSearch
+      onSearch(query);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSearch();
     }
   };
 
   return (
     <div className="header-container">
-      <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <form
+        className="search-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch();
+        }}
+      >
         <div className="search-container">
           <input
             className="search-input"
             type="text"
+            value={query}
             onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={handleKeyDown}
             placeholder="Введите поисковый запрос"
           />
-          <Button variant="contained" size="large" color="primary" onClick={handleSearch}>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            onClick={handleSearch}
+          >
             Искать
           </Button>
         </div>
